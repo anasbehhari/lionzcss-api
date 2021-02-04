64805dfb3226dc1724661d28b58551ce428e05e7
anasbehhari <an.behhari@gmail.com>
2021-02-04 01:04:36 +0100

    ini PORT

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get("/lionzcss/:folder/:file",(req,res) => {
         res.send("404 not found").status(404)
     }
 })
-app.listen(5000,(req,res) => {
+app.listen(process.env.PORT || 5000,(req,res) => {
     console.log("server ruuning");
 })
